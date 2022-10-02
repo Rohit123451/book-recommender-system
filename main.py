cd2d7225b9c261d152397e1a8fbcc7986b124458
Rohit123451 <97802377+Rohit123451@users.noreply.github.com>
2022-10-02 16:02:27 +0530

    favicon

--- main.py
+++ main.py
@@ -11,6 +11,10 @@ similarity_scores = pickle.load(open('similarity_scores.p', 'rb'))
 
 app = Flask(__name__)
 
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
 @app.route('/')
 def index():
     return render_template('index.html',
